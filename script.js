2c766fa3e1587b80b4ff41225d40bc1abec8e2cb
Rupesh11 <rupeshgurung11@gmail.com>
2026-01-17 16:30:07 +0000

    Understanding the concept of the flow and doing it myself

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-// DOM - Grabs element from HTML so it can speak to Javascript
 const firstNumberInput = document.getElementById("firstNumber");
 const secondNumberInput = document.getElementById("secondNumber");
 const operatorSelectInput = document.getElementById("operator");
 const calculateButton = document.getElementById("calculate");
 const resultParagraph = document.getElementById("result");
 
-// Brain of the calculator - It runs when you click a button
 function calculate() {
-  // It reads what the user typed, convert text into numbers and read which operator was selected
   const firstNumber = parseFloat(firstNumberInput.value);
   const secondNumber = parseFloat(secondNumberInput.value);
   const operator = operatorSelectInput.value;
   let result;
-  // It checks if the user type something else apart from number? If yes show error and stop function
+
   if (isNaN(firstNumber) || isNaN(secondNumber)) {
-    resultParagraph.textContent = "Please enter a valid number";
+    resultParagraph.textContent = "Please select the right operator";
     return;
   }
 
@@ -30,17 +27,15 @@ function calculate() {
       break;
     case "/":
       if (secondNumber === 0) {
-        resultParagraph.textContent = "Division by zero is not allowed";
+        resultParagraph.textContent = "Divison by Zero is not valid";
         return;
       }
       result = firstNumber / secondNumber;
       break;
-    default:
-      result = "Invalid operator";
-  }
 
-  // It prints answer to the screem
+    default:
+      result = "Invlalid operator";
+  }
   resultParagraph.textContent = "Result: " + result;
 }
-// When user click the button - It calls the function
 calculateButton.addEventListener("click", calculate);
